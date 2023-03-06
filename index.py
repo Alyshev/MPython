a2c9a0401b219c5cba5aa82583505185f59f2065
Alyshev <alyshiev.nikita@mail.ru>
2023-03-06 13:42:01 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,16 @@
 a = [4,3,4,2,1,2,4,6]
 
-
-
+count = int(0);
+sizeL = len(a)
 print(a)
 
-for i in range(len(a)-1):
-    if(a[i] % 2 == 0):
+for i in range(sizeL):
+    if(a[i] % 2 == 0 and count < 3):
         a.pop(i)
+        i -= 1
+        count += 1
+    else:
+        count = 0
+        
 
 print(a)
