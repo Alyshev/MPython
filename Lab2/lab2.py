e4633aee7e88af68e5554ab74d784859230c94d7
Alyshev <alyshiev.nikita@mail.ru>
2023-03-13 18:59:20 +0400

    Add numpy

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,26 +1,24 @@
 from random import randint
 import numpy as np
 
+
+
 highestValue = 0
 n = int(input())
 m = int(input())
 
-a = []
-for i in range(n):
-    a.append([0] * m) 
+arr = np.zeros((n, m), int)
 
 for i in range(n):
     for j in range(m):
-        a[i][j] = randint(0,10)
+        arr[i][j] = randint(0, 10)
 
-print(a)
+print(arr)
 for i in range(n):
-    tempValue = 0
-    for j in range(m):
-        tempValue += a[i][j]
-    print(tempValue, end = " ")
-    if(tempValue > highestValue):
-        highestValue = tempValue
-
+    tempHighestValue = arr[i].mean()
+    print(tempHighestValue, end = " ")
+    if (tempHighestValue > highestValue):
+        highestValue = tempHighestValue
 
 print('\n' + str(highestValue))
+
