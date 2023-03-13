a0ddc08052777875cefb54d58c231cc8721de87e
Alyshev <alyshiev.nikita@mail.ru>
2023-03-13 19:22:55 +0400

    Add file

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -2,23 +2,32 @@ from random import randint
 import numpy as np
 
 
-
 highestValue = 0
+print("Количество строк: ", end = "")
 n = int(input())
+print("Количество столбцов: ", end = "")
 m = int(input())
 
-arr = np.zeros((n, m), int)
+arr = np.zeros((n, m), int) # Массив заполненный нулями
 
 for i in range(n):
     for j in range(m):
         arr[i][j] = randint(0, 10)
 
+print("исходные данные:") 
 print(arr)
+
+# Нахождение максимального среднего значения из каждой строки матрицы
 for i in range(n):
     tempHighestValue = arr[i].mean()
-    print(tempHighestValue, end = " ")
     if (tempHighestValue > highestValue):
         highestValue = tempHighestValue
 
-print('\n' + str(highestValue))
+print("Результат обработки: " + str(highestValue))
 
+# Работа с файлом
+file = open("Lab2/result.txt", "w")
+file.write("исходные данные:\n") 
+file.write(str(arr))
+file.write("\nРезультат обработки: " + str(highestValue))
+file.close()
