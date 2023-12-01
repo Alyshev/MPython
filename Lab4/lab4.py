c06e1b40923c44a1a2cda2ba0e451531ba94bfd8
Alyshev <alyshiev.nikita@mail.ru>
2023-12-01 13:30:16 +0400

    Add generator, static, getitem, setatter, repr

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -10,38 +10,9 @@ class Files():
         self.files = [file for file in os.listdir(self.directory) if os.path.isfile(f'{self.directory}/{file}')]
         return len(self.files)
 
- 
-class Dictionaries():
-    def __init__(self):
-        self.employees = {}
-        self.temp = {}
-        self.point = 0
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.point >= len(self.employees.items()):
-            self.pointer = 0
-            raise StopIteration
-        else:
-            self.point += 1
-            return self.employees[self.temp[self.point - 1]]
-
-
-
-    def fillTheDictionary(self):
-        i = 0
-        with open('Lab3\R.csv', 'r', newline='', encoding='utf-8') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=';')
-            for number, fullName, position, seniority_years  in spamreader:
-                self.employees[int(number)] = [str(fullName), str(position), int(seniority_years)]
-                self.temp[i] = int(number)
-                i+=1
-
-    def returnDictionary(self):
-        return self.employees.items()              
-
+class sort():
+    employees = {}
     def sortByName(self):
         sorted_employees = dict(sorted(self.employees.items(), key=lambda item: item[1][0]))
         self.employees = sorted_employees
@@ -53,6 +24,63 @@ class Dictionaries():
     def sortByNumberGreaterThan20(self):
         sorted_employees = dict(filter(lambda item: item[0] > 20 , self.employees.items()))
         self.employees = sorted_employees
+
+
+class Dictionaries(sort):
+    def __init__(self):
+        self.employees = {}
+        self.temp = {}
+        self.point = 0
+
+    def __iter__(self):
+        self.point = 0
+        return self
+
+    def __next__(self):
+        if self.point >= len(self.employees.items()):
+            self.pointer = 0
+            raise StopIteration
+        else:
+            self.point += 1
+            return self.employees[self.temp[self.point - 1]]
+
+    def __repr__(self):
+        return f'Dictionaries({[repr(self.employees[val]) for val in self.employees]})'
+
+    def __setattr__(self, __name, __value):
+        self.__dict__[__name] = __value
+
+    def __getitem__(self, item):
+        if not isinstance(item, int):
+            raise TypeError('Error')
+        if 0 <= item < len(self.temp):
+            return self.employees[self.temp[item]] 
+        else:
+            raise IndexError('Error')
+
+    @staticmethod
+    def structureEmployees():
+        return "№, ФИО, должность, трудовой стаж"
+
+
+    def generator(self):
+        self.point = 0
+        while self.point < len(self.temp):
+            yield self.employees[self.temp[self.point]]
+            self.point += 1
+
+
+    def returnDictionary(self):
+        return self.employees.items()  
+
+    def fillTheDictionary(self):
+        i = 0
+        with open('Lab3\R.csv', 'r', newline='', encoding='utf-8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            for number, fullName, position, seniority_years  in spamreader:
+                self.employees[int(number)] = [str(fullName), str(position), int(seniority_years)]
+                self.temp[i] = int(number)
+                i+=1
 
     def loadDictionaryToFile(self):
         with open('Lab3\R.csv', 'w', encoding='utf-8') as csvfile:
@@ -69,10 +97,9 @@ print("")
 dictionary = Dictionaries()
 dictionary.fillTheDictionary()
 
-
 print("неотсортированный словарь:")
-for val in iter(dictionary):
-    print(val)
+for item in dictionary.generator():
+    print(item)
 print("")
 
 dictionary.sortByName()
